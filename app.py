bfc3f6e513959f8d4256c797d22c66317b28672d
manassinghrajput324-dot <manassinghrajput324@gmail.com>
2026-09-01 10:20:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template
-app = Flask(__name__)
+app = app = Flask(__name__, template_folder='.')
 
 BED_AVAILABILITY = {"General Wards":14,"ICU Wards":3,"Emergency Wards":5,"Special Wards":2}
 BLOOD_BANK_DATA = {
